e914fdc4e46cde032a2ae768c07f6707abf26b9b
Fabio Somaglia <fabio.somaglia@gmail.com>
2021-03-27 18:13:41 +0100

    Initial Commit

---
+++ Typing Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Typing Test
+//
+//  Created by Fabio Somaglia on 27/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Typing Test/Typing_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Typing_TestApp.swift
+//  Typing Test
+//
+//  Created by Fabio Somaglia on 27/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Typing_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
